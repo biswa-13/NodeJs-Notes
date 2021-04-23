7ce8b68798394060984380c874621de3869745a3
BiswaRanjan Samal <bichhubiswa@gmail.com>
2021-04-23 13:54:06 +0530

    Update 2_Node_SingleThreadRunningProblemDemo.js

--- 2_Node_SingleThreadRunningProblemDemo.js
+++ 2_Node_SingleThreadRunningProblemDemo.js
@@ -1,5 +1,8 @@
-// After running the application open http://localhost:3000/ in 2 browser tabs
-// Next open the browser console / netwroks tab and reload both the page simultaniously and look into the time taken for each request
+// Steps To run the program:
+// After running the application open http://localhost:3000/ in 1 browser tab 
+// and in another browser tab open http://localhost:3000/load url
+// Next open the browsers console / netwroks tab and reload both the page simultaniously and look into the time taken to serve each request
+// You can find that the second tab response gets served after the first request gets completed 
 // From here you can get to know that server is unable to process both the request parallely
 const express = require('express');
 const app = express();
@@ -14,9 +17,15 @@ function haultTheServer(duration, reqArrivalTime){
 }
 app.get('/', (req, res) => {
     const reqArrivalTime = new Date();
-    const duration = 5000;
+    const duration = 10000;
     haultTheServer(duration, reqArrivalTime);
-    res.send("Whoorey its working ...");
+    res.send(`Whoorey its working --> Req Came at ${reqArrivalTime}, Response Sent At :${new Date()}`);
+});
+
+app.get('/load', (req, res) => {
+    const reqArrivalTime = new Date();
+    res.send(`Loaded the /load route --> Req Came at ${reqArrivalTime}, Response Sent At :${new Date()}`);
 });
 
 app.listen(3000);
+
