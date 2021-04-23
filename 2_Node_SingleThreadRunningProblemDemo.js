9b5c9da309ff2c3a420beea2038c92df288f59fe
BiswaRanjan Samal <bichhubiswa@gmail.com>
2021-04-23 12:53:48 +0530

    Update 2_Node_SingleThreadRunningProblemDemo.js

--- 2_Node_SingleThreadRunningProblemDemo.js
+++ 2_Node_SingleThreadRunningProblemDemo.js
@@ -1,5 +1,5 @@
 // After running the application open http://localhost:3000/ in 2 browser tabs
-// Next open the browser console / netwroks tab and reload both the page and look into the time taken for each request
+// Next open the browser console / netwroks tab and reload both the page simultaniously and look into the time taken for each request
 // From here you can get to know that server is unable to process both the request parallely
 const express = require('express');
 const app = express();
